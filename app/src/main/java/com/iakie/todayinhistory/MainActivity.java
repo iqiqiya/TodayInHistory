fe047a8e3fe88d601b7829c94127b6c290f26831
iqiqiya <43666095+iqiqiya@users.noreply.github.com>
2019-12-25 15:12:39 +0800

    首页数据展示完成（包括历史上的今天和老黄历）

--- app/src/main/java/com/iakie/todayinhistory/MainActivity.java
+++ app/src/main/java/com/iakie/todayinhistory/MainActivity.java
@@ -4,16 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.iakie.todayinhistory.base.BaseActivity;
 import com.iakie.todayinhistory.base.ContentURL;
 import com.iakie.todayinhistory.bean.HistoryBean;
+import com.iakie.todayinhistory.bean.LaoHuangliBean;
 
+import org.xutils.http.RequestParams;
+import org.xutils.x;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -27,6 +34,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
     private HistoryAdapter adapter;
     private Calendar calendar;
     private Date date;
+    // 声明头布局当中的TextView
+    TextView yinliTv,dayTv,weekTv,yangliTv,baijiTv,wuxingTv,chongshaTv,jishenTv,xiongshenTv,yiTv,jiTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,8 +57,95 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
         int month = calendar.get(Calendar.MONTH) + 1;
         int day = calendar.get(Calendar.DAY_OF_MONTH);
 
+        addHeaderAndFooterView();
+
         String todayHistoryURl = ContentURL.getTodayHistory("1.0", month, day);
         loadData(todayHistoryURl);
+    }
+
+    private void addHeaderAndFooterView() {
+        // 给ListView添加头尾布局函数
+        View headerView = LayoutInflater.from(this).inflate(R.layout.main_headerview,null);
+        initHeaderView(headerView);
+        mainLv.addHeaderView(headerView);
+
+        View footerView = LayoutInflater.from(this).inflate(R.layout.main_footer,null);
+        mainLv.addFooterView(footerView);
+    }
+
+    private void initHeaderView(View headerView) {
+        // 初始化ListView
+        yinliTv = headerView.findViewById(R.id.main_header_tv_nongli);
+        dayTv = headerView.findViewById(R.id.main_header_tv_day);
+        weekTv = headerView.findViewById(R.id.main_header_tv_week);
+        yangliTv = headerView.findViewById(R.id.main_header_tv_yangli);
+        baijiTv = headerView.findViewById(R.id.main_header_tv_baiji);
+        wuxingTv = headerView.findViewById(R.id.main_header_tv_wuxing);
+        chongshaTv = headerView.findViewById(R.id.main_header_tv_chongsha);
+        jishenTv = headerView.findViewById(R.id.main_header_tv_jishen);
+        xiongshenTv = headerView.findViewById(R.id.main_header_tv_xiongshen);
+        yiTv = headerView.findViewById(R.id.main_header_tv_yi);
+        jiTv = headerView.findViewById(R.id.main_header_tv_ji);
+
+        // 将日期对象转换成指定格式的字符串形式
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String time = sdf.format(date);
+        String laoHuangliURL = ContentURL.getLaohuangliURL(time);
+        loadHeaderData(laoHuangliURL);
+    }
+
+    private void loadHeaderData(String laohuangliURL) {
+        // 获取老黄历接口的数据
+        RequestParams params = new RequestParams(laohuangliURL);
+        x.http().get(params, new CommonCallback<String>() {
+
+            @Override
+            public void onSuccess(String result) {
+                LaoHuangliBean huangliBean = new Gson().fromJson(result, LaoHuangliBean.class);
+                LaoHuangliBean.ResultBean resultBean = huangliBean.getResult();
+                yinliTv.setText("农历 "+resultBean.getYinli()+"（阴历）");
+                String[] yangliArr = resultBean.getYangli().split("-");
+                String week = getWeek(Integer.parseInt(yangliArr[0]),Integer.parseInt(yangliArr[1]),Integer.parseInt(yangliArr[2]));
+                yangliTv.setText("公历 "+yangliArr[0]+"年"+yangliArr[1]+"月"+yangliArr[2]+"日 "+week+"(阳历)");
+
+                dayTv.setText(yangliArr[2]);
+                weekTv.setText(week);
+                baijiTv.setText("彭祖百忌:"+resultBean.getBaiji());
+                wuxingTv.setText("五行:"+resultBean.getWuxing());
+                chongshaTv.setText("冲煞:"+resultBean.getChongsha());
+                jishenTv.setText("吉神宜趋:"+resultBean.getJishen());
+                xiongshenTv.setText("凶神宜忌"+resultBean.getXiongshen());
+                yiTv.setText("宜:"+resultBean.getYi());
+                jiTv.setText("忌:"+resultBean.getJi());
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
+    }
+
+    private String getWeek(int year, int month, int day) {
+        // 根据年月日获取对应的星期
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year,month-1,day);
+        String weeks[] = {"星期日","星期一","星期二","星期三","星期四","星期五","星期六"};
+        int index = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+        if (index<0) {
+            index = 0;
+        }
+        return weeks[index];
     }
 
     @Override
